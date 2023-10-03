23d861febbc075704fd74077f0c60cb62f60f71a
Kanav Ghai <kanghai@publicisgroupe.net>
2023-10-03 13:59:52 +0530

    chore: add a log statement

--- src/main/java/com/practice/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/practice/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.practice.microservices.currencyexchangeservice.controller;
 
 import com.practice.microservices.currencyexchangeservice.entity.ExchangeValue;
 import com.practice.microservices.currencyexchangeservice.repository.ExchangeValueRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,7 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
     @Autowired
     private Environment environment;
 
@@ -20,7 +23,8 @@ public class CurrencyExchangeController {
     private ExchangeValueRepository repository;
 
     @GetMapping("/currency-exchange")
-    public ExchangeValue getExchangeValue(@RequestParam String from, @RequestParam String to){
+    public ExchangeValue getExchangeValue(@RequestParam String from, @RequestParam String to) {
+        logger.info("getExchangeValue called from {} to {}", from, to);
         ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
         Integer port = Integer.parseInt(environment.getProperty("local.server.port"));
         exchangeValue.setPort(port);
